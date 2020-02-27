6e0cb20b36e89cfb12712f6d78b9c3229a8c212e
zhurof <zhuroff92@gmail.com>
2020-02-27 16:22:22 +0300

    ф

---
+++ basket.php
@@ -0,0 +1,117 @@
+﻿<?include "header.php";?>		
+	<main>
+		<div class="breadcrumbs">
+			<div class="wrapper">
+				<ul class="breadcrumbs__list">
+					<li><a href="/">Главная</a></li>
+					<li>Корзина</li>
+				</ul>
+			</div>
+		</div>
+		<div class="page-head">
+			<div class="wrapper">
+				<h1 class="page-header__title">Корзина</h1>
+				<a href="#" class="page-head__nav-link">Личный кабинет</a>
+			</div>
+		</div>
+		<div class="good-block">
+			<div class="wrapper">
+				<h2 class="good-block__title">Gunpowder and Lace Triangle Top & Skimpy Bottom</h2>
+				<div class="good-block__id">Артикул B18103T1 - B18103B1</div>
+				<div class="good-block__inner">
+					<div class="good-block__pic">
+						<img src="img/goods/2.jpg" alt="">						
+					</div>
+					<div class="good-block__text">
+						<div class="good-block__field">
+							<h4 class="good-block__key">Цвет</h4>
+							<div class="good-block__value">Чёрный</div>
+						</div>
+						<div class="good-block__field">
+							<h4 class="good-block__key">Размер</h4>
+							<div class="good-block__value">Лиф &nbsp;48</div>
+							<div class="good-block__value">Плавки &nbsp;52</div>							
+						</div>
+						<div class="good-block__price--old">92 900 ₽</div>
+						<div class="good-block__price">62 900 ₽</div>
+					</div>
+				</div>
+				<div class="good-block__tools">
+					<span class="icon-trash good-block__tool"></span>
+					<span class="icon-heart-o good-block__tool"></span>
+					<span class="number-field">
+						<span class="number-field__btn number-field__btn--minus">-</span>
+						<input type="text" class="number-field__input" value="1шт." readonly>
+						<span class="number-field__btn number-field__btn--plus">+</span>						
+					</span>
+				</div>
+			</div>
+		</div>
+		<div class="good-block">
+			<div class="wrapper">
+				<h2 class="good-block__title">Gunpowder and Lace Triangle Top & Skimpy Bottom</h2>
+				<div class="good-block__id">Артикул B18103T1 - B18103B1</div>
+				<div class="good-block__inner">
+					<div class="good-block__pic">
+						<img src="img/goods/3.jpg" alt="">						
+					</div>
+					<div class="good-block__text">
+						<div class="good-block__field">
+							<h4 class="good-block__key">Цвет</h4>
+							<div class="good-block__value">Чёрный</div>
+						</div>
+						<div class="good-block__field">
+							<h4 class="good-block__key">Размер</h4>
+							<div class="good-block__value">Лиф &nbsp;48</div>
+							<div class="good-block__value">Плавки &nbsp;52</div>							
+						</div>
+						<div class="good-block__price--old">92 900 ₽</div>
+						<div class="good-block__price">62 900 ₽</div>
+					</div>
+				</div>
+				<div class="good-block__tools">
+					<span class="icon-trash good-block__tool"></span>
+					<span class="icon-heart-o good-block__tool"></span>
+					<span class="number-field">
+						<span class="number-field__btn number-field__btn--minus">-</span>
+						<input type="text" class="number-field__input" value="1шт." readonly>
+						<span class="number-field__btn number-field__btn--plus">+</span>						
+					</span>
+				</div>
+			</div>
+		</div>
+		<div class="good-block">
+			<div class="wrapper">
+				<h2 class="good-block__title">Gunpowder and Lace Triangle Top & Skimpy Bottom</h2>
+				<div class="good-block__id">Артикул B18103T1 - B18103B1</div>
+				<div class="good-block__inner">
+					<div class="good-block__pic">
+						<img src="img/goods/2.jpg" alt="">						
+					</div>
+					<div class="good-block__text">
+						<div class="good-block__field">
+							<h4 class="good-block__key">Цвет</h4>
+							<div class="good-block__value">Чёрный</div>
+						</div>
+						<div class="good-block__field">
+							<h4 class="good-block__key">Размер</h4>
+							<div class="good-block__value">Лиф &nbsp;48</div>
+							<div class="good-block__value">Плавки &nbsp;52</div>							
+						</div>
+						<div class="good-block__price--old">92 900 ₽</div>
+						<div class="good-block__price">62 900 ₽</div>
+					</div>
+				</div>
+				<div class="good-block__tools">
+					<span class="icon-trash good-block__tool"></span>
+					<span class="icon-heart-o good-block__tool"></span>
+					<span class="number-field">
+						<span class="number-field__btn number-field__btn--minus">-</span>
+						<input type="text" class="number-field__input" value="1шт." readonly>
+						<span class="number-field__btn number-field__btn--plus">+</span>						
+					</span>
+				</div>
+			</div>
+		</div>
+	</main>
+<?include "footer.php";?>	
---
+++ good.php
@@ -0,0 +1,220 @@
+﻿<?include "header.php";?>		
+	<main>
+		<div class="breadcrumbs">
+			<div class="wrapper">
+				<ul class="breadcrumbs__list">
+					<li><a href="/">Главная</a></li>
+					<li><a href="catlog.php">Купальники</a></li>
+					<li>Jazmin Halter...</li>
+				</ul>
+			</div>
+		</div>
+		<div class="good">
+			<div class="wrapper">
+				<h1 class="good__title">Beach Bunny Swimwear Gunpowder and Lace High Neck Bikini</h1>
+				<div class="good__head">
+					<div class="good__category">Liliana Montoya Swim</div>
+					<div class="good__labels">
+						<div class="good__label good__label--white">ХИТ!</div>
+					</div>
+				</div>
+				<div class="good__slider">
+					<div class="good__slide"><img src="img/goods/1.jpg" alt=""></div>
+					<ul class="good__thumbs">
+						<li class="active"><img src="img/goods/1.jpg" alt=""></li>
+						<li><img src="img/goods/1-1.png" alt=""></li>
+						<li><img src="img/goods/1-2.png" alt=""></li>
+					</ul>
+				</div>				
+				<div class="good__price-block">
+					<span class="good__price">5 900 ₽</span>
+					<span class="good__price--old">8 900 ₽</span>
+				</div>
+				<div class="good__color">
+					<h4 class="good__color-title">Цвет | Ivory</h4>
+					<div class="good__color-palette">
+						<div class="good__color-item" style="border-color:#191E27;background-color:#F1EDEC;"></div>
+						<div class="good__color-item" style="background-color:#191E27;"></div>
+						<div class="good__color-item" style="background-color:#D49E7A;"></div>
+						<div class="good__color-item" style="background-color:#CFF1FF;"></div>
+						<div class="good__color-item" style="background-color:#FBE7B6;"></div>
+					</div>
+					
+				</div>
+				<div class="good__param">
+					<h4 class="good__param-title">Лиф</h4>
+					<h5 class="good__param-subtitle">Код товара B18103T1</h5>
+					<div class="good__param-inner">
+						<select class="good__param-input">
+							<option selected disabled>Размер</option>
+							<option>41</option>
+							<option>42</option>
+							<option>43</option>							
+						</select>
+						<a href="" class="good__param-link">Таблица размеров</a>
+					</div>
+				</div>
+				<div class="good__param">
+					<h4 class="good__param-title">Плавки</h4>
+					<h5 class="good__param-subtitle">Код товара B18103T1</h5>
+					<div class="good__param-inner">
+						<select class="good__param-input">
+							<option selected disabled>Размер</option>
+							<option>41</option>
+							<option>42</option>
+							<option>43</option>							
+						</select>
+						<a href="" class="good__param-link">Таблица размеров</a>
+					</div>
+				</div>
+				<div class="good__buttons">
+					<a href="" class="btn btn--cart btn--block good__btn">Положить в корзину</a>
+					<a href="" class="btn btn--heart btn--thin btn--block good__btn">Добавить в избранное</a>
+				</div>
+				<div class="good__note">Чтобы положить товар в корзину выберите параметры лифа и плавок.</div>
+				<div class="good__share">
+					<h4 class="good__share-title">Поделиться:</h4>
+					<div class="good__share-buttons">
+						<a href="" class="good__share-btn icon-instagram"></a>
+						<a href="" class="good__share-btn icon-facebook"></a>
+						<a href="" class="good__share-btn icon-vk"></a>
+					</div>
+				</div>
+				<div class="good__section">
+					<h3 class="good__section-title good__section-title--dropdown">Информация о товаре</h3>
+					<div class="good__section-content">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, quae, soluta enim distinctio quas sit modi. Quos, ad laborum nesciunt aspernatur nobis in porro molestiae fugiat dolor cum distinctio repudiandae!</p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, quae, soluta enim distinctio quas sit modi. Quos, ad laborum nesciunt aspernatur nobis in porro molestiae fugiat dolor cum distinctio repudiandae!</p>
+					</div>
+				</div>
+				<div class="good__section">
+					<h3 class="good__section-title"><a href="#">Условия оплаты и доставки</a></h3>
+				</div>
+			</div>
+		</div>
+		<div class="category-block">
+			<div class="wrapper">
+				<div class="category-block__head">
+					<h2 class="category-block__title category-block__title--simple">С этим товаром покупают</h2>
+				</div>
+				<div class="category-block__content">
+					<div class="good-card">
+						<div class="good-card__head">
+							<img src="img/goods/0.jpg" alt="" class="good-card__img">
+							<div class="good-card__info">
+								<div class="good-card__label">NEW!</div>
+								<div class="good-card__label good-card__label--white">ХИТ!</div>
+							</div>
+						</div>
+						<div class="good-card__body">
+							<div class="good-card__main">
+								<h3 class="good-card__title">Ariel Triangle Top & Tieside</h3>
+								<div class="good-card__category">Плавки</div>
+							</div>
+							<div class="good-card__footer">
+								<span class="good-card__price">2 900 ₽</span>
+							</div>
+						</div>
+					</div>
+					<div class="good-card">
+						<div class="good-card__head">
+							<img src="img/goods/1.jpg" alt="" class="good-card__img">
+							<div class="good-card__info">
+								<div class="good-card__label">NEW!</div>
+							</div>
+						</div>
+						<div class="good-card__body">
+							<div class="good-card__main">
+								<h3 class="good-card__title">Ariel Triangle Top & Tieside</h3>
+								<div class="good-card__category">Плавки</div>
+							</div>
+							<div class="good-card__footer">
+								<span class="good-card__price">2 900 ₽</span>
+							</div>
+						</div>
+					</div>
+					<div class="good-card">
+						<div class="good-card__head">
+							<img src="img/goods/0.jpg" alt="" class="good-card__img">
+							<div class="good-card__info">
+								<div class="good-card__label">NEW!</div>
+								<div class="good-card__label good-card__label--white">ХИТ!</div>
+							</div>
+						</div>
+						<div class="good-card__body">
+							<div class="good-card__main">
+								<h3 class="good-card__title">Ariel Triangle Top & Tieside</h3>
+								<div class="good-card__category">Плавки</div>
+							</div>
+							<div class="good-card__footer">
+								<span class="good-card__price">2 900 ₽</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="category-block">
+			<div class="wrapper">
+				<div class="category-block__head">
+					<h2 class="category-block__title category-block__title--simple">Вы недавно просматривали</h2>
+				</div>
+				<div class="category-block__content">
+					<div class="good-card">
+						<div class="good-card__head">
+							<img src="img/goods/0.jpg" alt="" class="good-card__img">
+							<div class="good-card__info">
+								<div class="good-card__label">NEW!</div>
+								<div class="good-card__label good-card__label--white">ХИТ!</div>
+							</div>
+						</div>
+						<div class="good-card__body">
+							<div class="good-card__main">
+								<h3 class="good-card__title">Ariel Triangle Top & Tieside</h3>
+								<div class="good-card__category">Плавки</div>
+							</div>
+							<div class="good-card__footer">
+								<span class="good-card__price">2 900 ₽</span>
+							</div>
+						</div>
+					</div>
+					<div class="good-card">
+						<div class="good-card__head">
+							<img src="img/goods/1.jpg" alt="" class="good-card__img">
+							<div class="good-card__info">
+								<div class="good-card__label">NEW!</div>
+							</div>
+						</div>
+						<div class="good-card__body">
+							<div class="good-card__main">
+								<h3 class="good-card__title">Ariel Triangle Top & Tieside</h3>
+								<div class="good-card__category">Плавки</div>
+							</div>
+							<div class="good-card__footer">
+								<span class="good-card__price">2 900 ₽</span>
+							</div>
+						</div>
+					</div>
+					<div class="good-card">
+						<div class="good-card__head">
+							<img src="img/goods/0.jpg" alt="" class="good-card__img">
+							<div class="good-card__info">
+								<div class="good-card__label">NEW!</div>
+								<div class="good-card__label good-card__label--white">ХИТ!</div>
+							</div>
+						</div>
+						<div class="good-card__body">
+							<div class="good-card__main">
+								<h3 class="good-card__title">Ariel Triangle Top & Tieside</h3>
+								<div class="good-card__category">Плавки</div>
+							</div>
+							<div class="good-card__footer">
+								<span class="good-card__price">2 900 ₽</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</main>
+<?include "footer.php";?>	
